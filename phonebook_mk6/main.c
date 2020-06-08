6cf59ac1494002bd5e37e6ec5b71957e5f194b06
JohnDohnut <51821505+JohnDohnut@users.noreply.github.com>
2020-06-08 20:58:47 +0900

    Update main.c

--- phonebook_mk6/main.c
+++ phonebook_mk6/main.c
@@ -2,7 +2,7 @@
 #include "phonebook.h"
 
 int main(int argc, char* argv[]){
-	phonebook_t* phonebook[1];
+	phonebook_t phonebook[1];
 	phonebook_init(phonebook,argc,argv);
 	phonebook_open_db(phonebook);
 	phonebook_read_db(phonebook);
